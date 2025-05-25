77e5a834a170793b73e48df4dfb10766ae89c768
Jeff Kloosterman <kloosterman.jeff@gmail.com>
2025-05-25 13:52:13 -0400

    Fix build

--- src/Router.tsx
+++ src/Router.tsx
@@ -25,7 +25,7 @@ export const router = createBrowserRouter([
           const setPackId = useAppStore((state) => state.setPackId)
           
           useEffect(() => {
-            clear()
+            clear(false)
             setPackId(undefined)
           })
           return (
